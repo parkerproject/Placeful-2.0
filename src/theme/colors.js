8dcd70c1dcd8e80c2a25a5d410a08e9c1a1daa2c
Parker <parkerproject@gmail.com>
2016-11-24 23:59:55 -0500

    new files

---
+++ src/theme/colors.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+export const DEFAULT_BACKGROUND_COLOR = 'rgb(250, 250, 250)'
+export const PRIMARY_COLOR = 'rgba(232, 45, 80, 1.0)'
+export const GRAY_COLOR = 'rgba(153, 153, 153, 1.0)'
+export const LIGHT_GRAY_COLOR = 'rgba(216, 216, 216, 1.0)'
+export const MEDIUM_GRAY_COLOR = 'rgba(204, 204, 204, 1.0)'
+export const BLACK_COLOR = 'rgba(51, 51, 51, 1.0)'
+export const WHITE_COLOR = 'rgba(255, 255, 255, 1.0)'
