466cdb0204ac23c0342d1aabfaff421cc75f13ed
Parker Ituk <parker.ituk@tscm101788.thestreet.com>
2016-11-30 16:58:27 -0500

    updated the launch screen

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -6,9 +6,9 @@ import { DEFAULT_BACKGROUND_COLOR } from '@theme/colors'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: DEFAULT_BACKGROUND_COLOR,
+    borderColor: 'red',
+    borderWidth: 1,
   },
 })
 
--- src/containers/LauchContainer/index.js
+++ src/containers/LauchContainer/index.js
@@ -1,21 +1,23 @@
 /* @flow */
 
-import React, { Component } from 'react'
-import { Platform } from 'react-native'
-import { Actions } from 'react-native-router-flux'
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import Container from '@components/Container'
-import Title from '@components/Title'
-import Link from '@components/Link'
+// import Title from '@components/Title'
 
-class LauchContainer extends Component<void, void, void> {
-  render() {
-    return (
-      <Container>
-        <Title>Hello there 😃 !</Title>
-        <Link onPress={Actions.counter}>Go to counter</Link>
-      </Container>
-    )
-  }
-}
-
-export default LauchContainer
+export default () => (
+  <Container>
+    <View style={{ flex: 0.7, borderColor: 'blue', borderWidth: 1 }}>
+      <Image
+        source={{ uri: 'https://unsplash.it/375/?random' }}
+        style={{ width: 375, height: 465 }}
+      />
+    </View>
+    <View style={{ flex: 0.3, borderColor: 'green', borderWidth: 1, alignItems: 'center' }}>
+      <View style={{ paddingVertical: 10 }}><Text>Experience amazing things</Text></View>
+      <Text style={{ fontSize: 10 }}>Let yourself driven by like-minded people</Text>
+      <Text style={{ fontSize: 10 }}>and get to experience the life</Text>
+      <Text style={{ fontSize: 10 }}>happening around you</Text>
+    </View>
+  </Container>
+)
